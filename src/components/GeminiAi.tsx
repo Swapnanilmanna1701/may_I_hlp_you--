f84ab02d7140068016ae68779c790292ed1637b8
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-20 02:32:09 +0530

    styling added

--- src/components/GeminiAi.tsx
+++ src/components/GeminiAi.tsx
@@ -130,25 +130,25 @@ const GeminiAI: React.FC = () => {
 
   return (
     <div className="flex flex-col min-h-svh items-center justify-between h-screen sm:p-3 lg:px-52 overflow-y-hidden pissoff">
-      <div className="h-full w-full flex flex-col items-center justify-center bg-slate-900 bg-opacity-90 backdrop-blur-md sm:rounded-lg overflow-y-scroll" style={{
+      <div className="h-full w-full flex flex-col items-center justify-center  bg-opacity-90 backdrop-blur-md sm:rounded-lg overflow-y-scroll" style={{
         scrollbarWidth: 'none',
       }}>
         {messages.length === 0 ? (
           <>
             <div className="flex flex-col justify-center items-center">
               <h1 className="text-gray-300 text-xl sm:text-2xl text-center tracking-wide font-bold mb-2">
-                Start chatting with your favorite
+                
               </h1>
               <h1 className="text-gray-300 text-xl sm:text-2xl tracking-wide font-bold">
-                <span className="text-transparent bg-gradient-to-br from-sky-400 via-blue-400 to-indigo-600 bg-clip-text text-4xl sm:text-5xl">Gemini</span>.AI here !!
+                <span className="text-transparent bg-gradient-to-br from-sky-400 via-blue-400 to-indigo-600 bg-clip-text text-4xl sm:text-5xl">May I Help You?</span>
               </h1>
 
               <Image
-                src="/robot.svg"
-                width={400}
-                height={400}
+                src="/bg.gif"
+                width={800}
+                height={800}
                 alt="Gemini AI"
-                className="mt-5 hover:scale-100 scale-90 transition-all duration-500 ease-in-out robot"
+                className="mt-5 hover:scale-100 scale-90 transition-all duration-500 ease-in-out robot sm:rounded-lg"
               />
             </div>
           </>
@@ -172,7 +172,7 @@ const GeminiAI: React.FC = () => {
                           } Avatar`}
                         src={
                           message.type === "ai"
-                            ? "/ai.gif"
+                            ? "/aii.gif"
                             : "/user.webp"
                         }
                       />
@@ -183,8 +183,8 @@ const GeminiAI: React.FC = () => {
                   </div>
                   <div
                     className={`chat-bubble max-w-full ${message.type === "ai"
-                      ? "bg-[#7480ff] text-black"
-                      : "bg-gray-950 text-white"
+                      ? "bg-gradient-to-br from-sky-400 via-blue-400 to-indigo-600 text-white"
+                      : "bg-gradient-to-br from-sky-400 via-blue-400 to-indigo-600 text-white"
                       }`}
                   >
                     <p className="text-sm sm:text-base font-medium"><MarkdownRenderer content={message.text} /></p>
@@ -198,7 +198,7 @@ const GeminiAI: React.FC = () => {
       </div>
 
       <div className="w-full flex flex-col items-start justify-center mt-5">
-        <div className="w-full flex items-center bg-slate-900 bg-opacity-90 backdrop-blur-md sm:rounded-md px-4 py-2">
+        <div className="w-full flex items-center bg-gradient-to-br from-sky-600 via-blue-800 to-indigo-900 bg-opacity-90 backdrop-blur-md sm:rounded-md px-4 py-2">
           <input
             type="text"
             value={inputValue}
@@ -213,10 +213,10 @@ const GeminiAI: React.FC = () => {
             type="submit"
             onClick={handleMessageSubmit}
             disabled={loading}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md ml-4 disabled:bg-gray-500"
+            className="bg-sky-400 text-white text-lg px-4 py-2 rounded-md ml-4 disabled:bg-gray-500"
           >
             {
-              loading ? <LuLoader className="animate-spin" size={24} /> : "Send"
+              loading ? <LuLoader className="animate-spin" size={28} /> : "Send"
             }
           </button>
         </div>
